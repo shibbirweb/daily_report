ef91557971dfc767ec31e3f76ebf6049adce6734
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2018-12-11 16:46:07 +0600

    Initial tempalte complete

--- index.php
+++ index.php
@@ -126,8 +126,8 @@
 					  </div>
 
 					  <div class="form-group row">
-					    <div class="col-sm-10">
-					      <button type="submit" class="btn btn-primary text-center">Genarate</button>
+					    <div class="col-sm-10 offset-sm-1 text-center">
+					      <button type="submit" class="btn btn-primary">Genarate</button>
 					    </div>
 					  </div>
 					</form>
